1acf5514a1ccd6476c6240635958184a3905ff76
richiii999 <richiii@umich.edu>
2026-03-24 19:38:41 -0400

    WIP placeTile()

--- Game.py
+++ Game.py
@@ -20,7 +20,7 @@ class SudokuGame:
         self.solution = states[1]
 
         # Metadata
-        self.gamedata = Gamedata.Gamedata()
+        self.meta = Gamedata.Gamedata()
 
     def prettyPrint(self, grid, wall='|', floor='-', empty='.'):
         """Prints the full Sudoku board with formatting"""
@@ -30,8 +30,24 @@ class SudokuGame:
             txtRow = [N if N != 0 else empty for N in row]
             print(f"{txtRow[0]} {txtRow[1]} {txtRow[2]} {wall} {txtRow[3]} {txtRow[4]} {txtRow[5]} {wall} {txtRow[6]} {txtRow[7]} {txtRow[8]}")
 
+    def PlaceTile(self, row, col, val):
+        """Attempts to place val at [row][col]"""
+        if row < 0 or row > 8: raise ValueError("Invalid row")
+        if col < 0 or col > 8: raise ValueError("Invalid col")
+        if val < 0 or val > 9: raise ValueError("Invalid val")
+
+        if self.initial[row][col] != 0: return # Cant change initial tiles
+
+        self.curr[row][col] = val
+
+        if val != 0 and not self.solution[row][col] == val: self.meta.mistakes += 1
 
 # TESTING
 
 g1 = SudokuGame(RNG=12)
-g1.prettyPrint(g1.initial)
+g1.prettyPrint(g1.curr)
+
+print("Placing 7 @ 0,0")
+g1.PlaceTile(0,0,7)
+g1.prettyPrint(g1.curr)
+print(f"Mistakes={g1.meta.mistakes}")
--- Gamedata.py
+++ Gamedata.py
@@ -7,6 +7,6 @@ import time
 class Gamedata:
     def __init__(self):
         self.time = -1 # How long the game took (in sec)
-        self.mistakesCount = -1 # Num placements not in solution
-        self.notesCount = -1 # Num notes added
+        self.mistakes = -1 # Num placements not in solution
+        self.notes = -1 # Num notes added
         self.difficulty = -1 # Num empty squares started with
