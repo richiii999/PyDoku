341b5c547121f5724877279fe24f88d1b93ccee9
richiii999 <richiii@umich.edu>
2026-03-29 12:21:54 -0400

    win func WIP

--- Game.py
+++ Game.py
@@ -2,12 +2,10 @@
 # Game.py
 # Stores the current game state and has functions to play the game
 
-from copy import deepcopy
-
 import Generator # Generate Sudoku initial and solution states
 import Gamedata # Metadata struct
 from bisect import insort # Used for inserting to lists in-order
-from copy import deepcopy
+from copy import deepcopy # Deepcopy the board since its nested lists
 
 class SudokuGame:
     def __init__(self, difficulty = 40, RNG=None):
@@ -17,7 +15,7 @@ class SudokuGame:
         self.initial = deepcopy(states[0])
         self.curr = states[0]
         self.solution = states[1]
-        self.notes = [ [ [] for _ in range(9) ] for _ in range(9)] 
+        self.notes = [ [ [] for _ in range(9) ] for _ in range(9)]
         # Notes structure kinda bloated tbh
 
         # Metadata
@@ -26,7 +24,7 @@ class SudokuGame:
     def prettyPrint(self, grid=None, wall='|', floor='-', empty='.', info=False) -> None:
         """Prints the full Sudoku board with formatting"""
         if grid is None: grid = self.curr
-        
+
         for row in grid:
             if grid.index(row) != 0 and grid.index(row) % 3 == 0: print(f"{floor * 21}") # Horz separator
             txtRow = [N if N != 0 else empty for N in row]
@@ -51,10 +49,13 @@ class SudokuGame:
             print("Duplicate, removing tile")
             self.curr[row][col] = 0
 
-        self.curr[row][col] = val
-
         if val != 0 and not self.solution[row][col] == val:
             self.meta.numMistakes += 1
+
+        self.curr[row][col] = val
+
+        if self.CheckIfWin(): self.Win()
+
 
     def AddNote(self, row, col, val) -> None:
         """Attempts to place val at [row][col]"""
@@ -71,14 +72,26 @@ class SudokuGame:
             print("Duplicate, removing note")
             self.notes[row][col].remove(val)
 
-        else: 
+        else:
             insort(self.notes[row][col], val) # Insert in-order
             self.meta.numNotes += 1
 
     def CheckIfWin(self) -> bool:
         """Returns T/F if board is solved"""
         return self.curr == self.solution
-    
+
+    def Win(self) -> None:
+        """Stops the game and submits it to the database if won"""
+        print("Win!")
+        if not self.CheckIfWin(): raise RuntimeError("Tried to win an unsolved board!")
+
+        # TODO
+        # Stop timer
+        # Save metadata to DB
+
+
+
+
 
 # TESTING
 
@@ -116,4 +129,6 @@ print(f"Slots={total}")
 print("TEST: Solving board")
 g1.curr = g1.solution
 g1.prettyPrint(info=True)
-print(f"Solved={g1.CheckIfWin()}")
+g1.PlaceTile(0,0,1) # Removing a tile
+g1.PlaceTile(0,0,8) # Placing the final tile
+# Should win here
