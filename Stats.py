bdc208c7ff7985bdf50d0446b3dfbb7c026c6410
richiii999 <richiii@umich.edu>
2026-03-29 12:53:35 -0400

    stats WIP

---
+++ Stats.py
@@ -0,0 +1,27 @@
+
+# Stats.py: Contains functions to graph stats of games in the DB
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+# TODO
+# Connect to db and read the games
+
+# TEMP: replace with real data once DB done
+data = pd.read_csv("fakedata.csv")
+
+def GamesByTime():
+    """Graphs games by ID / Time (Chronological play order)"""
+    sns.scatterplot(x="ID", y="Time", data=data, hue="Time")
+
+    plt.title("Time per game")
+    # x/y label already set from DB
+    plt.show()
+
+
+
+
+
+# TESTING
+GamesByTime()
